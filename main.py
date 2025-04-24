7ec1d5e81f5a1a5ae5a5a0663b997e7de30e208f
Hexte <lucijan.hrastnik@gmail.com>
2025-04-24 22:04:21 +0200

    added found article scraping

--- main.py
+++ main.py
@@ -8,29 +8,29 @@ import glob
 async def main():
     tasks = []
     scrape_targets = [
-        {
-            "name": "cryptonews",
-            "url": "https://cryptonews.com/news/"
-        },
-        {
-            "name": "coindesk",
-            "url": "https://www.coindesk.com/latest-crypto-news/"
-        },
+        # {
+        #     "name": "cryptonews",
+        #     "url": "https://cryptonews.com/news/"
+        # },
+        # {
+        #     "name": "coindesk",
+        #     "url": "https://www.coindesk.com/latest-crypto-news/"
+        # },
         {
             "name": "beincrypto",
             "url": "https://beincrypto.com/news/"
         },
         ]
 
-    scrape_targets_dump = json.dumps(scrape_targets)
-    print()
-    # print(scrape_targets)
+    # for i in range(scrape_targets.__len__()):
+    #     tasks.append(scrapeForArticles(scrape_targets[i]["name"], scrape_targets[i]["url"]))
+    # await asyncio.gather(*tasks)
+    # tasks = []
     for i in range(scrape_targets.__len__()):
-        tasks.append(scrapeArticles(scrape_targets[i]["name"],scrape_targets[i]["url"]))
+        tasks.append(scrapeArticles(scrape_targets[i]["name"]))
     await asyncio.gather(*tasks)
 
-
-async def scrapeArticles(name, url):
+async def scrapeForArticles(name, url):
     async with AsyncWebCrawler() as crawler:
         with open("mainSchemas/schema_" + name + ".json", 'r', encoding='utf-8') as file:
             schema = json.load(file)
@@ -42,7 +42,6 @@ async def scrapeArticles(name, url):
             extraction_strategy=extraction_strategy,
         )
 
-
         result = await crawler.arun(
             url=url,
             config=config
@@ -51,6 +50,45 @@ async def scrapeArticles(name, url):
         data = json.loads(result.extracted_content)
         with open('outputs/output_' + name + ".json", 'w') as f:
             f.write(json.dumps(data, indent=2))
+
+async def scrapeArticles(name):
+    async with AsyncWebCrawler() as crawler:
+        with open("articleSchemas/article_schema_" + name + ".json", 'r', encoding='utf-8') as file:
+            schema = json.load(file)
+            print(schema)
+
+        with open("outputs/output_" + name + ".json", 'r', encoding='utf-8') as file:
+            scrapedArticleList = json.load(file)
+            # print(scrapedArticleList)
+        extraction_strategy = JsonCssExtractionStrategy(schema, verbose=True, log_console=True)
+
+        config = CrawlerRunConfig(
+            cache_mode=CacheMode.BYPASS,
+            extraction_strategy=extraction_strategy,
+        )
+
+        urls = []
+        for i in range(scrapedArticleList.__len__()):
+            # print(scrapedArticleList[i]["link"])
+            urls.append(scrapedArticleList[i]["link"])
+
+        result = await crawler.arun_many(
+            urls=urls,
+            config=config
+        )
+        for article in result:
+            for x in article:
+                data = json.loads(x.extracted_content)
+                title = data[0]["title"]
+                title = ''.join(e for e in title if e.isalnum())
+                savepath = 'articles/' + name + "/" + title + ".json"
+                print(data)
+                with open(savepath, 'w') as f:
+                    f.write(json.dumps(data, indent=2))
+        # for article in result:
+        #     data = json.loads(article.extracted_content)
+        #     with open('articles/_' + name + ".json", 'w') as f:
+        #         f.write(json.dumps(data, indent=2))
 
 if __name__ == "__main__":
     asyncio.run(main())
