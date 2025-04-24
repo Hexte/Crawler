c6e6cbb738c57121b905cd7470142ab0dd1d11c8
Hexte <lucijan.hrastnik@gmail.com>
2025-04-24 19:03:40 +0200

    scraping multiple pages at once for articles

--- main.py
+++ main.py
@@ -3,33 +3,39 @@ from crawl4ai import *
 from crawl4ai.deep_crawling import BFSDeepCrawlStrategy
 from crawl4ai.deep_crawling.scorers import KeywordRelevanceScorer
 import json
+import glob
 
 async def main():
+    tasks = []
+    scrape_targets = [
+        {
+            "name": "cryptonews",
+            "url": "https://cryptonews.com/news/",
+            "schemaName": "schema_cryptonews.com.json"
+        },
+        {
+            "name": "coindesk",
+            "url": "https://www.coindesk.com/latest-crypto-news/",
+            "schemaName": "schema_coindesk.com.json"
+        },
+        {
+            "name": "beincrypto",
+            "url": "https://beincrypto.com/news/",
+            "schemaName": "schema_beincrypto.com.json"
+        },
+        ]
+
+    scrape_targets_dump = json.dumps(scrape_targets)
+    print()
+    # print(scrape_targets)
+    for i in range(scrape_targets.__len__()):
+        tasks.append(scrapeArticles(scrape_targets[i]["name"],scrape_targets[i]["url"], scrape_targets[i]["schemaName"]))
+    await asyncio.gather(*tasks)
+
+
+async def scrapeArticles(name, url, schemaName):
     async with AsyncWebCrawler() as crawler:
-        # schema = {
-        #             "name": "cryptonews article",
-        #             "baseSelector": ".archive-template-latest-news__wrap",
-        #             "fields": [
-        #             {
-        #                 "name": "title",
-        #                 "selector": ".archive-template-latest-news__title",
-        #                 "type": "text"
-        #             },
-        #             {
-        #                 "name": "link",
-        #                 "selector": ".archive-template-latest-news",
-        #                 "type": "attribute",
-        #                 "attribute": "href"
-        #             },
-        #             {
-        #                 "name": "time",
-        #                 "selector": ".archive-template-latest-news__time",
-        #                 "type": "attribute",
-        #                 "attribute": "data-utctime"
-        #             }
-        #             ]
-        #         }
-        with open('schema_cryptonews.com.json', 'r', encoding='utf-8') as file:
+        with open(schemaName, 'r', encoding='utf-8') as file:
             schema = json.load(file)
             print(schema)
         extraction_strategy = JsonCssExtractionStrategy(schema, verbose=True)
@@ -39,14 +45,18 @@ async def main():
             extraction_strategy=extraction_strategy,
         )
 
+
         result = await crawler.arun(
-            url="https://cryptonews.com/news/",
+            url=url,
             config=config
         )
 
         data = json.loads(result.extracted_content)
-        print(f"Extracted {len(data)} entries")
-        print(json.dumps(data, indent=2) if data else "No data found")
+        # print(f"Extracted {len(data)} entries")
+        # print(json.dumps(data, indent=2))
+        with open('outputs/output_' + name, 'w') as f:
+            f.write(json.dumps(data, indent=2))
 
 if __name__ == "__main__":
     asyncio.run(main())
+    # asyncio.run(scrapeArticles("beincrypto","https://beincrypto.com/news/", "schema_beincrypto.com.json"))
