8c78a4541fbace186222a6fe907ee76d64ccba06
Hexte <lucijan.hrastnik@gmail.com>
2025-05-06 18:02:51 +0200

    kafka integracija

--- main.py
+++ main.py
@@ -4,31 +4,70 @@ from crawl4ai.deep_crawling import BFSDeepCrawlStrategy
 from crawl4ai.deep_crawling.scorers import KeywordRelevanceScorer
 import json
 import glob
+from kafka import KafkaProducer
+import os
 
 async def main():
     tasks = []
     scrape_targets = [
-        # {
-        #     "name": "cryptonews",
-        #     "url": "https://cryptonews.com/news/"
-        # },
-        # {
-        #     "name": "coindesk",
-        #     "url": "https://www.coindesk.com/latest-crypto-news/"
-        # },
+        {
+            "name": "cryptonews",
+            "url": "https://cryptonews.com/news/"
+        },
+        {
+            "name": "coindesk",
+            "url": "https://www.coindesk.com/latest-crypto-news/"
+        },
         {
             "name": "beincrypto",
             "url": "https://beincrypto.com/news/"
         },
         ]
 
-    # for i in range(scrape_targets.__len__()):
-    #     tasks.append(scrapeForArticles(scrape_targets[i]["name"], scrape_targets[i]["url"]))
-    # await asyncio.gather(*tasks)
-    # tasks = []
+    for i in range(scrape_targets.__len__()):
+        tasks.append(scrapeForArticles(scrape_targets[i]["name"], scrape_targets[i]["url"]))
+    await asyncio.gather(*tasks)
+    tasks = []
     for i in range(scrape_targets.__len__()):
         tasks.append(scrapeArticles(scrape_targets[i]["name"]))
     await asyncio.gather(*tasks)
+
+    sendScrapedToKafka("articles")
+
+def sendScrapedToKafka(path):
+    producer = KafkaProducer(
+        bootstrap_servers='127.0.0.1:29092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+
+    for filename in os.listdir(path):
+        if filename.endswith('.json'):
+            filepath = os.path.join(path, filename)
+
+            try:
+                with open(filepath, 'r') as f:
+                    data = json.load(f)
+
+                    if all(field in data for field in ['title', 'synopsys', 'content', 'link', 'time']):
+                        future = producer.send(
+                            "scraped_data",
+                            value=data
+                        )
+
+                        try:
+                            record_metadata = future.get(timeout=10)
+                            print("sent")
+                        except Exception as e:
+                            print(f"error: {str(e)}")
+
+            except json.JSONDecodeError:
+                print(f"Skipping {filename}: Invalid JSON format")
+            except Exception as e:
+                print(f"Error processing {filename}: {str(e)}")
+
+
+    producer.flush()
+    producer.close()
 
 async def scrapeForArticles(name, url):
     async with AsyncWebCrawler() as crawler:
@@ -82,6 +121,10 @@ async def scrapeArticles(name):
                 title = data[0]["title"]
                 title = ''.join(e for e in title if e.isalnum())
                 savepath = 'articles/' + name + "/" + title + ".json"
+                data[0]["link"] = x.url
+                for i in range(scrapedArticleList.__len__()):
+                    if scrapedArticleList[i]["link"] == x.url:
+                        data[0]["time"] = scrapedArticleList[i]["time"]
                 print(data)
                 with open(savepath, 'w') as f:
                     f.write(json.dumps(data, indent=2))
